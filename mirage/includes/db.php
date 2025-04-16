5cd5acb51071143317b55d480591639510df94cf
0xZenVal <casey_ng21@hotmail.com>
2025-04-17 06:43:02 +0800

    Add mirage/includes/db.php 1744843379603

---
+++ mirage/includes/db.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'config.php';
+
+try {
+    $db = new PDO(DB_DSN, DB_USER, DB_PASS);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Database connection failed: " . $e->getMessage();
+    exit();
+}
+?>
