5e27bce240cc276bf642892727b6ffdbdf74f2e9
0xZenVal <casey_ng21@hotmail.com>
2025-04-17 07:49:57 +0800

    Add mirage/includes/config.php 1744847395298

--- mirage/includes/config.php
+++ mirage/includes/config.php
@@ -1,7 +1,13 @@
 <?php
+session_start();
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'mirage_db');
 define('DB_USER', 'your_mysql_username');
 define('DB_PASS', 'your_mysql_password');
 define('DB_DSN', 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME);
+
+// Error reporting for development
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 ?>
